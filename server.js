6070c31a3ac184f9cdec6d8f5d3b317f69dcf69b
Edward Diaz <diazedward24@gmail.com>
2024-01-09 11:00:26 -0800

    Set up server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log(`Server is live! Listening at port ${PORT}`);
+});
