2caa9bcd2ee4fa453de9a112f86aaa8fd47aafa2
Edward Diaz <diazedward24@gmail.com>
2024-01-09 18:06:53 -0800

    Add body parser

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 
 app.get('/', function (req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-
+app.post('/users', (req, res) => {
+    console.log(req.body);
+});
 
 
 app.listen(PORT, function() {
