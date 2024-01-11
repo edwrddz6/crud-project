dcb19405c4ec84be3f489e320c0df021a7cbf4aa
Edward Diaz <diazedward24@gmail.com>
2024-01-11 11:26:36 -0800

    Refactor html to ejs, render data from server side to client side

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { error } = require('console');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}))
+require('dotenv').config({path: '.env'});
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
-MongoClient.connect('connection');
+MongoClient.connect(process.env.MONGO_URI)
+    .then(client => {
+        const db = client.db('practice');
+        const usersCollection = db.collection('users');
 
-app.get('/', function (req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+        app.get('/', (req, res) => {
+            usersCollection
+                .find()
+                .toArray()
+                .then(results => {
+                    res.render('index.ejs', { usersCollection: results });
+                })
+                .catch(error => console.error(error))
+        })
 
-app.post('/users', (req, res) => {
-    console.log(req.body);
-});
+        app.post('/users', (req, res) => {
+            usersCollection
+                .insertOne(req.body)
+                .then(result => {
+                    res.redirect('/');
+                })
+            .catch(error => console.error(error))
+        });
+        
+        app.listen(PORT, function() {
+            console.log(`Server is live! Listening at port ${PORT}`);
+        });
+    })
+    .catch(error => console.error(error));
 
 
-app.listen(PORT, function() {
-    console.log(`Server is live! Listening at port ${PORT}`);
-});
